1a84867335cbaea1a4b24585ae5afc32b4e6d5c0
Isaac-bse <isaackiprono@zetech.ac.ke>
2026-08-25 06:58:33 +0300

    Add files via upload

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+$host = "localhost";
+$dbname = "student_portal";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Database Connection Failed: " . $e->getMessage());
+}
+?>
